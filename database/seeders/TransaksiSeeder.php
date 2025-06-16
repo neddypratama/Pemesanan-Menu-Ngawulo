3a6a2eb4034379928789cd5c39df898a2ac69556
neddypratama <neddypratama92@gmail.com>
2025-06-16 15:08:33 +0700

    done

--- database/seeders/TransaksiSeeder.php
+++ database/seeders/TransaksiSeeder.php
@@ -21,7 +21,7 @@ class TransaksiSeeder extends Seeder
                 'user_id' => rand(1, 10), // Pastikan ada user dengan ID 1-10
                 'tanggal' => Carbon::now()->subDays(rand(0, 30)),
                 'total' => rand(50000, 500000), // Total transaksi dalam rentang tertentu
-                'no_meja' => rand(1, 10),
+                // 'no_meja' => rand(1, 10),
                 'status' => ['success', 'pending', 'cancel'][rand(0, 2)], // Status acak
                 'created_at' => Carbon::now()->subDays(rand(0, 30)),
                 'updated_at' => Carbon::now()->subDays(rand(0, 30)),
